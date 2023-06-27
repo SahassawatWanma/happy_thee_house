8853cf8567e8cecd00932dd00814395ebd1119b0
SahassawatWanma <91061456+SahassawatWanma@users.noreply.github.com>
2023-06-27 09:15:21 +0700

    create movies table

---
+++ database/migrations/2023_06_27_020808_create_movies_table.php
@@ -0,0 +1,26 @@
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMoviesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->integer('release_year');
+            $table->string('genre');
+            $table->decimal('price', 8, 2);
+            $table->string('image_url')->nullable();
+            $table->string('trailer_url')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
