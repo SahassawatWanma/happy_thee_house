cb2a938e632760df3152075f717724d63ed5c984
SahassawatWanma <91061456+SahassawatWanma@users.noreply.github.com>
2023-06-27 09:17:42 +0700

    Create 2023_06_27_021702_create_orders_table.php

---
+++ database/migrations/2023_06_27_021702_create_orders_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('movie_id');
+            $table->dateTime('purchase_date');
+            $table->decimal('total_amount', 8, 2);
+            $table->string('status');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('movie_id')->references('id')->on('movies');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
